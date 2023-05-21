f28ac333a777afe6a7de3e3a39fcc3f382b1d97c
Pierre-Henri <pierreh37@gmail.com>
2023-05-21 20:09:31 +0200

    update to use tsup

--- dist/Identifier.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const FrameLoopManagerName: unique symbol;
-export declare const AnimationFrameLoopFactoryName: unique symbol;
-export declare const SetIntervalLoopFactoryName: unique symbol;
-export declare const SetTimeoutLoopFactoryName: unique symbol;
-export declare const TimeLoggerName: unique symbol;
-//# sourceMappingURL=Identifier.d.ts.map
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,13 +1,96 @@
-import { AxModule } from "axolotis-module-definition";
-import { ContainerModule } from "inversify";
-export * from "./services/loop/FrameLoopManager";
-export * from "./services/loop/AnimationFrameLoopFactory";
-export * from "./services/loop/LoopInterface";
-export * from "./services/loop/SetIntervalLoopFactory";
-export * from "./services/loop/SetTimeoutLoopFactory";
-export * from "./services/perf/TimeLogger";
-export * from "./Identifier";
-export declare class AxLoopModule implements AxModule {
+import { AxModule } from 'axolotis-module-definition';
+import { ContainerModule } from 'inversify';
+
+interface LoopInterface {
+    start(): void;
+    stop(): void;
+}
+type LoopCallback = (delta: number) => void;
+interface LoopRemoverInterface {
+    loop: LoopInterface;
+    remover: () => void;
+}
+
+declare class FrameLoopManager {
+    started: boolean;
+    constructor();
+    loops: Map<string, LoopInterface>;
+    start(): void;
+    stop(): void;
+    addLoop(name: string, loop: LoopInterface): () => void;
+    removeLoop(name: string): void;
+}
+
+interface TimeLoggerInterface {
+    monitoringStart: () => void;
+    monitoringEnd: () => void;
+}
+declare const SUFFIX_DELIMITER = "-";
+declare class TimeLogger {
+    getTimeLogger(name: string): TimeLoggerInterface;
+    callbacks: ({
+        refreshInterval: number;
+        cb: (label: string, time: number, duration: number, minTime: number, maxTime: number, sampleNumber: number, totalTimeMs: number) => void;
+    })[];
+    onPerfLog(minimumRefreshInterval: number, callback: (label: string, time: number, duration: number, minTime: number, maxTime: number, sampleNumber: number, totalTimeMs: number) => void): () => void;
+    enablePerfLog(activated: boolean): void;
+}
+
+declare class AnimationFrameLoop implements LoopInterface {
+    private callback;
+    private timeLogger;
+    enable: boolean;
+    constructor(callback: LoopCallback, timeLogger: TimeLoggerInterface);
+    start(): void;
+    stop(): void;
+}
+declare class AnimationFrameLoopFactory {
+    private timeLogger;
+    private frameLoop;
+    constructor(timeLogger: TimeLogger, frameLoop: FrameLoopManager);
+    create(name: string, callback: LoopCallback): LoopRemoverInterface;
+}
+
+declare class SetIntervalLoop implements LoopInterface {
+    private callback;
+    private timeLogger;
+    private intervalMs;
+    private intervalId;
+    constructor(callback: LoopCallback, timeLogger: TimeLoggerInterface, intervalMs: number);
+    start(): void;
+    stop(): void;
+}
+declare class SetIntervalLoopFactory {
+    private timeLogger;
+    private frameLoop;
+    constructor(timeLogger: TimeLogger, frameLoop: FrameLoopManager);
+    create(name: string, callback: LoopCallback, intervalMs?: number): LoopRemoverInterface;
+}
+
+declare class SetTimeoutLoop implements LoopInterface {
+    private callback;
+    private timeLogger;
+    private timeoutMs;
+    enable: boolean;
+    constructor(callback: LoopCallback, timeLogger: TimeLoggerInterface, timeoutMs: number);
+    start(): void;
+    stop(): void;
+}
+declare class SetTimeoutLoopFactory {
+    private timeLogger;
+    private frameLoop;
+    constructor(timeLogger: TimeLogger, frameLoop: FrameLoopManager);
+    create(name: string, callback: LoopCallback, timeoutMs?: number): LoopRemoverInterface;
+}
+
+declare const FrameLoopManagerName: unique symbol;
+declare const AnimationFrameLoopFactoryName: unique symbol;
+declare const SetIntervalLoopFactoryName: unique symbol;
+declare const SetTimeoutLoopFactoryName: unique symbol;
+declare const TimeLoggerName: unique symbol;
+
+declare class AxLoopModule implements AxModule {
     getModule(): ContainerModule;
 }
-//# sourceMappingURL=index.d.ts.map
+
+export { AnimationFrameLoop, AnimationFrameLoopFactory, AnimationFrameLoopFactoryName, AxLoopModule, FrameLoopManager, FrameLoopManagerName, LoopCallback, LoopInterface, LoopRemoverInterface, SUFFIX_DELIMITER, SetIntervalLoop, SetIntervalLoopFactory, SetIntervalLoopFactoryName, SetTimeoutLoop, SetTimeoutLoopFactory, SetTimeoutLoopFactoryName, TimeLogger, TimeLoggerInterface, TimeLoggerName };
